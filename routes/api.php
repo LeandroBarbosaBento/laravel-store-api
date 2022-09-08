4aebe50dd1c38a25c01ef2419d07ef08c1893bb4
LeandroBarbosaBento <67109002+LeandroBarbosaBento@users.noreply.github.com>
2022-09-08 14:47:21 -0300

    Configuring route api.php file

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\CategoryController;
+use \App\Http\Controllers\ProductController;
+use \App\Http\Controllers\OrderController;
+use \App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('users', UserController::class);
